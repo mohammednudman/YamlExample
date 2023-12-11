3cf96b1d6ed182d5f86260daec114af27033726b
Mohammed Nudman Raza Shaikh <mohammednudman@gmail.com>
2023-12-11 11:51:26 +0530

    Updated README.md

--- tests/main-test.cpp
+++ tests/main-test.cpp
@@ -3,34 +3,33 @@
 
 // Test the Employee class
 TEST(EmployeeTest, Constructor) {
-std::string csvLine = "John Doe,123,2022-01-01,Engineering";
-Employee employee(csvLine);
+    std::string csvLine = "John Doe,123,2022-01-01,Engineering";
+    Employee employee(csvLine);
 
-EXPECT_EQ(employee.name, "John Doe");
-EXPECT_EQ(employee.id, "123");
-
-// Assuming you have set the joining_date_str correctly in the test data
-EXPECT_EQ(employee.joining_date.tm_year, 122); // 2022 - 1900
-EXPECT_EQ(employee.department, "Engineering");
+    EXPECT_EQ(employee.name, "John Doe");
+    EXPECT_EQ(employee.id, "123");
+    EXPECT_EQ(employee.joining_date.tm_year, 122); // 2022 - 1900
+    EXPECT_EQ(employee.department, "Engineering");
 }
 
 // Test the EmployeeBonusCalculator class
 TEST(EmployeeBonusCalculatorTest, CalculateLoyaltyBonus) {
-std::filesystem::path csvFilePath = "C:\\Users\\Mohammed\\CLionProjects\\untitled\\Employee.csv";
-std::string yamlFilePath = "C:\\Users\\Mohammed\\CLionProjects\\untitled\\BonusRange.yaml";
+    std::filesystem::path csvFilePath = "C:\\Users\\Mohammed\\CLionProjects\\untitled\\Employee.csv";
+    std::string yamlFilePath = "C:\\Users\\Mohammed\\CLionProjects\\untitled\\BonusRange.yaml";
 
-// Create a sample bonus range configuration for testing
-YAML::Node bonus_range_config;
-bonus_range_config["RangeAmountConfig"]["1"] = 100;
-bonus_range_config["RangeAmountConfig"]["3"] = 200;
-bonus_range_config["RangeAmountConfig"]["5"] = 300;
+    YAML::Node bonus_range_config = YAML::LoadFile(yamlFilePath)["RangeAmountConfig"];
 
-EmployeeBonusCalculator calculator(csvFilePath, yamlFilePath);
+    EmployeeBonusCalculator calculator(csvFilePath, yamlFilePath);
 
-// Assuming you have a valid calculateLoyaltyBonus implementation
-int bonus = EmployeeBonusCalculator::calculateLoyaltyBonus({2022, 0, 1, 0, 0, 0}, {2023, 0, 1, 0, 0, 0}, bonus_range_config);
+    // Use current date and a date one year ago for testing
+    std::time_t current_time = std::time(nullptr);
+    std::tm *current_date = std::localtime(&current_time);
+    std::tm one_year_ago = *current_date;
+    one_year_ago.tm_year -= 1;
 
-EXPECT_EQ(bonus, 100); // Bonus for 1 year of work
+    int bonus = EmployeeBonusCalculator::calculateLoyaltyBonus(one_year_ago, *current_date, bonus_range_config);
+
+    EXPECT_EQ(bonus, 50000); // Bonus for 1 year of work
 }
 
 int main(int argc, char **argv) {
